53b41c13c9e973b0934c3e7dc1017a089675b8fe
Johnathan Gilday <me@johnathangilday.com>
2024-09-09 22:49:16 -0400

    🚧 Add Dark Typography and Background

--- src/components/blog/typography.tsx
+++ src/components/blog/typography.tsx
@@ -1,11 +1,26 @@
 import Typography from "typography"
 import theme from "typography-theme-sutro"
 
-theme.overrideThemeStyles = () => {
-  return {
+theme.overrideThemeStyles = ({ rhythm }, options, styles) => {
+  const darkModeStyles = {
+    body: {
+      backgroundColor: "#121212",
+      color: "#ffffff",
+    },
     "a.gatsby-resp-image-link": {
       boxShadow: `none`,
     },
+  }
+
+  const lightModeStyles = {
+    "a.gatsby-resp-image-link": {
+      boxShadow: `none`,
+    },
+  }
+
+  return {
+    ...lightModeStyles,
+    "@media (prefers-color-scheme: dark)": darkModeStyles,
   }
 }
 
